19e8ade41df40077aaea63117fd8117040c588a4
Théo Turlin <theo.turlin.22@gmail.com>
2018-09-19 17:41:41 +0200

    Adding the calculation of the radial acceleration

--- ALPHA/spacecraft.py
+++ ALPHA/spacecraft.py
@@ -17,3 +17,7 @@ class Spacecraft():
 
     def display_spacecraft(self):
         plt.polar(self.theta, self.r, '+', c=self.color)
+
+    def acceleration(self, black_hole):
+        self.ar = - (c.G * black_hole.mass * c.M_SOL)/(self.r**2 * black_hole.rs)\
+                +(self.r * black_hole.rs - 3/2*black_hole.rs)*(self.l0**2/(self.r**4 * black_hole.rs**4))
