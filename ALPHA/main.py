21af6ba19282858b906491cf7e8acf27b9013abd
Théo Turlin <theo.turlin.22@gmail.com>
2018-09-20 14:58:19 +0200

    Add the game loop

--- ALPHA/main.py
+++ ALPHA/main.py
@@ -5,20 +5,55 @@ import matplotlib.pyplot as plt
 from spacecraft import Spacecraft
 from black_hole import Black_hole
 
-dist = float(input("Distance to the event horizon , in Rs : "))
-angle = float(input("Angle from axis, in rad : "))
-vt = float(input("Tangential speed, in percentage of c : "))
-vr = float(input("Radial speed, in percentage of c : "))
+dist = 0
+angle = 'q'
+vt = 0
+vr = 0
+
+while dist <= 1. or dist > 20.:
+    try:
+        dist = float(input("Distance to the event horizon , in Rs : "))
+    except:
+        pass
+
+while type(angle) != type(1.):
+    try:
+        angle = float(input("Angle from axis, in rad : "))
+    except:
+        pass
+
+while 0. >= vt or vt >= 1.:
+    try:
+        vt = float(input("Tangential speed, in percentage of c : "))
+    except:
+        pass
+
+while 0. >= vr or vr >= 1.:
+    try:
+        vr = float(input("Radial speed, in percentage of c : "))
+    except:
+        pass
+
 
 hole = Black_hole(100)
 player = Spacecraft(dist, angle, vr, vt, 'b')
-foe = Spacecraft(round(float(np.random.uniform(1, 10)), 3),\
+foe = Spacecraft(round(float(np.random.uniform(1, 15)), 3),\
                  player.theta + np.pi, round(float(np.random.rand()), 3),\
                  round(float(np.random.rand()), 3), 'r')
 
 
-hole.event_horizon_display()
-player.display_spacecraft()
-foe.display_spacecraft()
+while not player.loose and not foe.loose:
+    player.display_spacecraft()
+    hole.event_horizon_display()
+    foe.display_spacecraft()
 
-plt.show()
+    player.leapfrog(hole)
+    foe.leapfrog(hole)
+    player.display_trajectory()
+    foe.display_trajectory()
+    plt.show()
+
+if player.loose:
+    print("you loose")
+else:
+    print("you win")
